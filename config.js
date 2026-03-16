d0149d5bfb208c56f1c3b8d6cf1a8fe3945115b4
Daniil <danil_nikishkin@inbox.ru>
2026-03-16 13:04:02 +0300

    avito review links

---
+++ config.js
@@ -0,0 +1,11 @@
+window.APP_CONFIG = {
+  reviewLinks: {
+    default: "https://www.avito.ru/user/review?fid=2_OgIbTjf5vuM9OL93PlDXQOjowHx4ZBZ87DElF8B0nlyL6RdaaYzvyPSWRjp4ZyNE",
+    overrides: [
+      {
+        utmSource: "openwrt_avito",
+        url: "https://www.avito.ru/user/review?fid=2_P2tcfmGvurW_wTDv-Tkh67hWDPStYC2KY2omVO4Nl6eIU3-wIqRK3nRBp4dTnSSc"
+      }
+    ]
+  }
+};
