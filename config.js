49f0a8c1b3f741b986a81b48aca6e9cb762c4ab9
Daniil <danil_nikishkin@inbox.ru>
2026-05-07 20:16:02 +0300

    tg and buy added

--- config.js
+++ config.js
@@ -7,5 +7,23 @@ window.APP_CONFIG = {
         url: "https://www.avito.ru/user/review?fid=2_P2tcfmGvurW_wTDv-Tkh67hWDPStYC2KY2omVO4Nl6eIU3-wIqRK3nRBp4dTnSSc"
       }
     ]
+  },
+  buyRouterLinks: {
+    default: "https://www.avito.ru/brands/i163848794",
+    overrides: [
+      {
+        utmSource: "openwrt_avito",
+        url: "https://www.avito.ru/user/7f08dc01ddb19be58d2ff0ebdab93d67/profile?src=search_seller_info&iid=7964894037"
+      }
+    ]
+  },
+  telegramLinks: {
+    default: "https://t.me/nikishkindan",
+    overrides: [
+      {
+        utmSource: "openwrt_avito",
+        url: "https://t.me/chumerov"
+      }
+    ]
   }
 };
